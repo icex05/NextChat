38870d0056bca2a09f68ece209b06d84b925a4f5
icex05 <164710061+icex05@users.noreply.github.com>
2025-10-19 02:50:47 -0500

    Update google.ts

--- app/client/platforms/google.ts
+++ app/client/platforms/google.ts
@@ -149,6 +149,8 @@ export class GeminiProApi implements LLMApi {
       ...{
         model: options.config.model,
       },
+    delete modelConfig.presence_penalty;
+    delete modelConfig.frequency_penalty;
     };
     const requestPayload = {
       contents: messages,
